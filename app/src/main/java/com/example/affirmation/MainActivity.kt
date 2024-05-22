6bcd50381e7fba031f477213c43c0f2839a7fcd0
dankedj <dankedj18@gmail.com>
2024-05-22 13:31:04 +0100

    Creation de la liste deroulante

--- app/src/main/java/com/example/affirmation/MainActivity.kt
+++ app/src/main/java/com/example/affirmation/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -23,6 +25,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.affirmation.data.Affirmation
+import com.example.affirmation.data.DataSource
 import com.example.affirmation.ui.theme.AffirmationTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,12 +45,18 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun AffirmationApplication(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AffirmationList(affirmations:List<Affirmation>,modifier:Modifier){
+
+    LazyColumn(modifier=modifier) {
+        items(affirmations){
+            affirmation -> AffirmationCarte(
+            affirmationObjet = affirmation,
+            modifier=Modifier.padding(8.dp)
+            )
+        }
+    }
 }
 @Composable
 fun AffirmationCarte(affirmationObjet:Affirmation,modifier:Modifier=Modifier){
